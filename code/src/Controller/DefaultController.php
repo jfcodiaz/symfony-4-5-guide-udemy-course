08ea4e3072a84fce2c8ddd516656faba5f43f1a7
gituser <email@gmail.com>
2021-04-28 05:44:00 +0000

    make a controller with make command

--- code/src/Controller/DefaultController.php
+++ code/src/Controller/DefaultController.php
@@ -1,11 +1,20 @@
 <?php
+
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController {
-
-    public function indeX(){
-        return new Response('<p>Hello Wordl');
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/default", name="default")
+     */
+    public function index(): Response
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
     }
 }
