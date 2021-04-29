e37ad94534d98515b57b7954bbb6fb87c51d12b0
gituser <email@gmail.com>
2021-04-29 05:12:20 +0000

    Import a third party service [Logger]

--- code/src/Services/GiftsService.php
+++ code/src/Services/GiftsService.php
@@ -2,19 +2,29 @@
 
 namespace App\Services;
 
+use Psr\Log\LoggerInterface;
+
 class GiftsService {
     
     private $gifts = ['flowers', 'car', 'piano', 'money'];
 
-    public function __construct() {
-        shuffle($this->gifts);
+    private $loger;
+
+    public function __construct(LoggerInterface $logger) {
+        $this->logger = $logger;    
+        $this->randomize();
     }
 
     public function setGifts($gifts) {
         $this->gifts = $gifts;
-        shuffle($this->gifts);
+        $this->randomize();
     }
 
+    private function randomize() {
+        $this->logger->info("Gifts were randomized!");
+        shuffle($this->gifts);
+    }
+    
     public function __invoke() {
         return $this->gifts;
     }
